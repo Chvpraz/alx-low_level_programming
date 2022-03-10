b6a781dc13a3822630b74e9e19b2d4565d8c3916
Chvpraz <samuel.ayoadeniyi@gmail.com>
2022-03-10 15:57:11 -0800

    omo ogbon

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 int main(void)
+/**
+ * main - went to print texts,you will found out
+ *
+ * Return: Always 0.
+ */
 {
-	puts("\"programmming is like building a multilingual puzzle");
+	puts("\"programming is like building a multilingual puzzle");
 
 	return (0);
 }
