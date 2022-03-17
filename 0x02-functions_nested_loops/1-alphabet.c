63b2bd119f4750424bf67eb0f4e5cbfdc5f9ea07
Chvpraz <samuel.ayoadeniyi@gmail.com>
2022-03-17 11:12:36 -0700

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+##include "alx.h"
+
+/**
+ * print out the alphabet
+ * main - use built in function
+ * Return: 0 if successful
+ */
+
+void print_alphabet(void)
+{
+char i;
+for (i = 'a'; i <= 'z'; 1++)
+_putchar(i);
+_putchar('\n');
+}
