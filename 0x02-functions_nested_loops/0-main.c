f0a0ccb77f2b9b893a1bff60a69f4584ac834432
Chvpraz <samuel.ayoadeniyi@gmail.com>
2022-03-18 03:46:42 -0700

    done

---
+++ 0x02-functions_nested_loops/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - print holberton
+ * Return:0 if successful
+ */
+int main(void)
+{
+_putchar('M');
+_putchar('a');
+_putchar('i');
+_putchar('n');
+_putchar('\n');
+
+return (0);
+}
