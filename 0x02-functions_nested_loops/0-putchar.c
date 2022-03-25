f1db31d1c7250a6f6b0cbc9c838a11e58e776a8b
Chvpraz <samuel.ayoadeniyi@gmail.com>
2022-03-25 15:03:47 -0700

    leggo

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,23 @@
-#include <stdio.h>
+#include <main.h>
 
 /**
- * main - Print _purchar
- * Return: 0 if successful
+ * main - Entry point
+ *
+ * Return:nothing, return void 
  */
+
 int main(void)
 {
 
-printf('_purchar\n');	
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+
 return (0);
 }
