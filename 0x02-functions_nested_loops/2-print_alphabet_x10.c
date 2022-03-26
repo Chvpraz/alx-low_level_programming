a50e23e67608bfd091ec7ab403856ca10e2f9b86
Chvpraz <samuel.ayoadeniyi@gmail.com>
2022-03-26 15:45:59 -0700

    wowza

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,19 +6,19 @@
  * return: Always 0.
  */
  void print_alphabet_x10(void) 
-  {
-  int alphabet;
-  int count;
+ {
+ int alphabet;
+ int count;
  
-  count = 0;
-  while (count < 10)
-  {
-  for (alphabet = 'a' ; alphabet <= 'z'; alphabet++)
-  {
+ count = 0;
+ while (count < 10)
+ {
+ for (alphabet = 'a' ; alphabet <= 'z'; alphabet++)
+ {
   _putchar(alphabet);
-  }
-  count++;
-  _putchar('\n');
-  }
- 
-  }
+ }
+ count++;
+ _putchar('\n');
+ }
+
+ }
